7cd4168f5ca41693318f77757a8908a694954897
Jaakko Kantojärvi <jaakko@n-1.fi>
2013-02-14 18:42:57 +0200

    Introduce trusted omniauth
    Trusted auth is implementation to group all different strong or trusted authentication methods under same code and policy.
    
    Trusted authentications for now are ldap, pam, cas(?) and shibboleth(?). From these ldap and pam works similarly. They ask username and password for authentication. So to implement shibboleth, some modifications are required.

--- spec/lib/auth_spec.rb
+++ spec/lib/auth_spec.rb
@@ -4,7 +4,8 @@ describe Gitlab::Auth do
   let(:gl_auth) { Gitlab::Auth.new }
 
   before do
-    Gitlab.config.stub(omniauth: {})
+    Gitlab.config.stub(trusted_omniauth: {}, omniauth: {})
+    Gitlab.config.trusted_omniauth.stub(:provider).and_return('ldap')
 
     @info = mock(
       uid: '12djsak321',
@@ -49,34 +50,48 @@ describe Gitlab::Auth do
       @auth = mock(
         info: @info,
         provider: 'twitter',
-        uid: '12djsak321',
+        uid: @info.uid,
+      )
+      @ldap_auth = mock(
+        info: @info,
+        provider: 'ldap',
+        uid: @info.uid,
       )
     end
 
-    it "should find user"do
+    it "should find user" do
       User.should_receive :find_by_provider_and_extern_uid
       gl_auth.should_not_receive :create_from_omniauth
       gl_auth.find_or_new_for_omniauth(@auth)
     end
 
-    it "should not create user"do
+    it "should not create user" do
       User.stub find_by_provider_and_extern_uid: nil
       gl_auth.should_not_receive :create_from_omniauth
       gl_auth.find_or_new_for_omniauth(@auth)
     end
 
+    it "should create user with trusted auth" do
+      User.stub find_by_provider_and_extern_uid: nil
+      gl_auth.should_receive :create_from_omniauth
+      gl_auth.find_or_new_for_omniauth(@ldap_auth)
+    end
+
     it "should create user if single_sing_on"do
-      Gitlab.config.omniauth['allow_single_sign_on'] = true
+      Gitlab.config.omniauth.stub(:allow_single_sign_on).and_return(true)
       User.stub find_by_provider_and_extern_uid: nil
       gl_auth.should_receive :create_from_omniauth
       gl_auth.find_or_new_for_omniauth(@auth)
     end
+
+    # FIXME: test find_by_email
+    # FIXME: Omniauth::Error when no uid
   end
 
   describe :create_from_omniauth do
     it "should create user from LDAP" do
-      @auth = mock(info: @info, provider: 'ldap')
-      user = gl_auth.create_from_omniauth(@auth, true)
+      # create_from_omniauth is private
+      user = gl_auth.send(:create_from_omniauth, @info.uid, @info.email, @ldap_auth)
 
       user.should be_valid
       user.extern_uid.should == @info.uid
@@ -84,12 +99,15 @@ describe Gitlab::Auth do
     end
 
     it "should create user from Omniauth" do
-      @auth = mock(info: @info, provider: 'twitter')
-      user = gl_auth.create_from_omniauth(@auth, false)
+      # create_from_omniauth is private
+      user = gl_auth.send(:create_from_omniauth, @info.uid, @info.email, @auth)
 
       user.should be_valid
       user.extern_uid.should == @info.uid
       user.provider.should == 'twitter'
     end
+
+    # FIXME: test block_auto_created_users
+    # FIXME: test Omniauth::Error when no email
   end
 end
