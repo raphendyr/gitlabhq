71fd11c125a05846ee4fbd02320e0daab39d7de0
Jaakko Kantojärvi <jaakko@n-1.fi>
2014-01-21 18:38:32 +0200

    squash! Update comment in lib/gitlab/pam/user.rb

--- lib/gitlab/pam/user.rb
+++ lib/gitlab/pam/user.rb
@@ -24,8 +24,9 @@ module Gitlab
             #
             # Possible cases:
             # * When user already has account and need to link his PAM account.
-            # * PAM uid changed for user with same email and we need to update his uid
             #
+            # Only trust email, do not try to find with same username
+            # admin is needed to handle username collisions
             user = model.find_by_email(email)
 
             if user
