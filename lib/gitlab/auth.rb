ef1ccd9be5a9daa64e90dbabe00cf4a1f6bd97c1
Jaakko Kantojärvi <jaakko@n-1.fi>
2014-01-28 21:04:13 +0200

    Clean gitlab/auth.rb and make sure ldap_conf is ok before using

--- lib/gitlab/auth.rb
+++ lib/gitlab/auth.rb
@@ -5,20 +5,10 @@ module Gitlab
 
       if user.nil? || user.ldap_user?
         # Second chance - try LDAP authentication
-        return nil unless ldap_conf.enabled
-
         Gitlab::LDAP::User.authenticate(login, password)
       else
         user if user.valid_password?(password)
       end
-    end
-
-    def log
-      Gitlab::AppLogger
-    end
-
-    def ldap_conf
-      @ldap_conf ||= Gitlab.config.ldap
     end
   end
 end
